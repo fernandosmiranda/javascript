8c7b03ee9423c0f478d2f76ff5be4bae0af7c189
Fernando Miranda <55719707+fernandosmiranda@users.noreply.github.com>
2025-12-23 17:40:06 -0300

    Aula 11

---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+let pais = 'EUA';
+console.log(`Sendo seu país de origem o(a) ${pais}.`);
+
+if (pais == 'Brasil'){
+    console.log(`Você é Brasileiro`);
+} else {
+    console.log(`Você é Estrangeiro`);
+}
