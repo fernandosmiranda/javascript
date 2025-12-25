842fea048c8509705195a31628c88c50a35448cf
Fernando Miranda <55719707+fernandosmiranda@users.noreply.github.com>
2025-12-25 15:48:32 -0300

    Aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+// console.log("Tudo bem?")
+// console.log("Tudo bem?")
+// console.log("Tudo bem?")
+// console.log("Tudo bem?")
+// console.log("Tudo bem?")
+// console.log("Tudo bem?")
+
+/*
+let x = 1
+
+while (x <= 6){
+   console.log(`Passo ${x}`)
+   x++ 
+}
+*/
+
+let x = 1
+do {
+    console.log(`Passo ${x}`)
+    x++
+} while (x <= 6)
