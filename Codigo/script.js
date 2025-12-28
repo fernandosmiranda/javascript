b317eac364c472e6fb35bd5714acea62728983e8
Fernando Miranda <55719707+fernandosmiranda@users.noreply.github.com>
2025-12-28 12:02:41 -0300

    Exemplos de Codigos

---
+++ Codigo/script.js
@@ -0,0 +1,7 @@
+let atributoDiv = document.getElementById("idQualquer")
+
+// atributoDiv.setAttribute('style', 'background: grey; border: 1px solid black; font-size: 1.5em;')
+
+atributoDiv.style.background = 'grey';
+atributoDiv.style.border = '1px solid black';
+atributoDiv.style.fontSize = '1.5em';
